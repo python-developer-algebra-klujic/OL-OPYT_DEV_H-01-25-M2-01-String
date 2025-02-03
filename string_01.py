be1337005a45f4c7f89df0c95f3d1de71472a980
python-developer-algebra-klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-02-03 20:31:31 +0100

    Update string_01.py

--- string_01.py
+++ string_01.py
@@ -1,9 +1,10 @@
-first_name = 'Pero'
-last_name = 'Peric'
-age = 28
+first_name = input('Upisite  Vase ime: ')
+last_name = input('Upisite  Vase prezime: ')
+age = int(input('Upisite  Vasu dob: '))
+
 
 # 'Dobro dosli! Prijavljeni ste kao ' + last_name + ' ' + first_name + ', (' + age + ').'
 # message = 'Dobro dosli! Prijavljeni ste kao {} {}, ({}).'.format(first_name, last_name, age)
-message = f'Dobro dosli! Prijavljeni ste kao {last_name} {first_name}, ({age}).'
+message = f'Dobro dosli! Prijavljeni ste kao {last_name.capitalize()} {first_name.capitalize()}, ({age}).'
 
 print(message)
