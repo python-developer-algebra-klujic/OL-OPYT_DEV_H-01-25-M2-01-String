9cfc75277d7be99cb64143280b6c2d7721a5aaba
python-developer-algebra-klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-02-03 20:40:39 +0100

    Create string_02.py

---
+++ string_02.py
@@ -0,0 +1,8 @@
+# koristimo posebni znak \ koji se zove ESCAPE CHARACTER
+
+# message = 'That\'s not correct!'
+# message = "That's not correct!"
+# message = "\"Kruno\", that is not correct!"
+message = '\t\t"Kruno", that\'s not correct!\n'
+
+print(message)
